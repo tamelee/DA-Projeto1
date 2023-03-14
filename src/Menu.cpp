b0456a52eb6d918ce0fce4459aec6fdff0e387ea
Elton Tamele <elton.tamel@gmail.com>
2023-03-14 11:20:54 +0000

    App Menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,25 +1,29 @@
 #include "Menu.h"
 
 void Menu::appMenu() {
-    cout << "********************************" << endl;
-    cout << "*                              *" << endl;
-    cout << "*    Railway                   *" << endl;
-    cout << "*      Network                 *" << endl;
-    cout << "*                              *" << endl;
-    cout << "*     1) DATA SET FILES        *" << endl;
-    cout << "*     2) OPERATIONS            *" << endl;
-    cout << "*     0) SAIR                  *" << endl;
-    cout << "*                              *" << endl;
-    cout << "*                              *" << endl;
-    cout << "********************************" << endl;
+    cout << "******************************************" << endl;
+    cout << "*                                        *" << endl;
+    cout << "*        Railway                         *" << endl;
+    cout << "*          Network                       *" << endl;
+    cout << "*                                        *" << endl;
+    cout << "*     1) MANAGE DATA                     *" << endl;
+    cout << "*     2) BASIC SERVICE METRICS           *" << endl;
+    cout << "*     3) OPERATION COST OPTIMIZATION     *" << endl;
+    cout << "*     4) RELIABILITY AND SENSITIVITY     *" << endl;
+    cout << "*     0) EXIT                            *" << endl;
+    cout << "*                                        *" << endl;
+    cout << "*                                        *" << endl;
+    cout << "******************************************" << endl;
     int option;
     do{
         cin >> option;
         switch(option) {
             case 1: break;
             case 2: break;
+            case 3: break;
+            case 4: break;
             case 0: exit(0);
-            default: cout << "Opcao invalida, por favor tente novamente: ";
+            default: cout << "Invalid option, please try again: ";
         }
-    } while(option != 0 && option != 1 && option != 2);
+    } while(option != 0 && option != 1 && option != 2 && option != 3 && option != 4);
 }
